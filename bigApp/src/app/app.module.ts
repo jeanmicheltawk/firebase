61a9864ee6c302b8ebd2d5e050da234a2d13cab5
jeanmicheltawk <jean-michel.tawk@greybell.com>
2020-10-27 12:33:13 +0200

    jeanmichel

---
+++ bigApp/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { AboutComponent } from './components/about/about.component';
+import { RegisterComponent } from './components/register/register.component';
+import { CardviewComponent } from './components/cardview/cardview.component';
+import { ListViewComponent } from './components/list-view/list-view.component';
+import { MapViewComponent } from './components/map-view/map-view.component';
+import { NewCustomerComponent } from './components/new-customer/new-customer.component';
+import { NewOrderComponent } from './components/new-order/new-order.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    AboutComponent,
+    RegisterComponent,
+    CardviewComponent,
+    ListViewComponent,
+    MapViewComponent,
+    NewCustomerComponent,
+    NewOrderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ bigApp/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: 'AIzaSyCLK4jFdrvA-J44u2GY7t9SaTNpEgqcoYw',
+    authDomain: 'bigtest-7ce40.firebaseapp.com',
+    databaseURL: 'https://bigtest-7ce40.firebaseio.com',
+    projectId: 'bigtest-7ce40',
+    storageBucket: 'bigtest-7ce40.appspot.com',
+    messagingSenderId: '602411512200',
+    appId: '1:602411512200:web:01616e524e3fda0c7a4a3d'
+  }
+};
