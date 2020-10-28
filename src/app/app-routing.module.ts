5b5f45c66c62471a939070823fe3b2eb0d06dc82
jeanmicheltawk <jean-michel.tawk@greybell.com>
2020-10-28 15:17:41 +0200

    v3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AboutComponent } from './components/about/about.component';
-import { CustomersComponent } from './components/customers/customers.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LoginComponent } from './components/login/login.component';
-import { OrdersComponent } from './components/orders/orders.component';
 import { RegisterComponent } from './components/register/register.component';
 
 
 const routes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
   {path: 'register', component: RegisterComponent},
-  {path: 'customer', component: CustomersComponent},
-  {path: 'about', component: AboutComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'orders', component: OrdersComponent},
+  {path: 'dashboard', component: DashboardComponent}
 ];
 
 @NgModule({
