538884858df47819daae48ae56073b8550f896fb
Habib SAADE <habib.saade@greybell.com>
2020-10-29 09:37:55 +0200

    day2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,14 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { FormsModule } from '@angular/forms';
+
 import { environment } from 'src/environments/environment';
-=======
+
 import { CustomersComponent } from './components/customers/customers.component';
 import { OrdersComponent } from './components/orders/orders.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
->>>>>>> origin/jeanmichel
-=======
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 import { DashboardModule } from './components/dashboard/dashboard.module';
->>>>>>> jeanmichel
 
 @NgModule({
   declarations: [
@@ -33,19 +26,10 @@ import { DashboardModule } from './components/dashboard/dashboard.module';
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-<<<<<<< HEAD
-    AngularFireModule.initializeApp(environment.firebaseConfig),
-    NgbModule
-=======
     NgbModule,
     FormsModule,
-<<<<<<< HEAD
-    ReactiveFormsModule
->>>>>>> origin/jeanmichel
-=======
     ReactiveFormsModule,
     DashboardModule
->>>>>>> jeanmichel
   ],
   providers: [],
   bootstrap: [AppComponent]
