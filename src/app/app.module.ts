2a3b1077a8ea391245a3e44084af30a85a2b2b3f
Habib SAADE <habib.saade@greybell.com>
2020-10-29 18:12:13 +0200

    habibs changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AngularFireModule} from '@angular/fire';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-import { environment } from 'src/environments/environment';
 
 import { CustomersComponent } from './components/customers/customers.component';
 import { OrdersComponent } from './components/orders/orders.component';
@@ -40,11 +38,8 @@ import { environment } from 'src/environments/environment';
     ReactiveFormsModule,
     DashboardModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
-<<<<<<< HEAD
-=======
     AngularFireAuthModule,
     AngularFirestoreModule
->>>>>>> origin/jeanmichel
   ],
   providers: [RegisterComponent],
   bootstrap: [AppComponent]
